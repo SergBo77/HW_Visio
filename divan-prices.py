6844d270c0f00a0ad2558c0418ec836d784d209b
SergBo77 <seman77@mail.ru>
2024-07-04 21:22:31 +0300

    add comments

--- divan-prices.py
+++ divan-prices.py
@@ -1,3 +1,4 @@
+# Импортируем библиотеки
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
@@ -5,29 +6,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+# Инициализация драйвера Chrome
 driver = webdriver.Chrome()
 
+# Открытие страницы для поиска диванов на сайте divan.ru
 driver.get('https://www.divan.ru/search?ProductSearch%5Bname%5D=%D0%B4%D0%B8%D0%B2%D0%B0%D0%BD&no_cache=1')
 
+# Ожидание загрузки страницы
 time.sleep(3)
 
-prices = driver.find_elements(By.XPATH,"//span[@class='ui-LD-ZU KIkOH']")
+# Нахождение элементов с ценами диванов
+prices = driver.find_elements(By.XPATH, "//span[@class='ui-LD-ZU KIkOH']")
 
 price = []
 
+# Извлечение и обработка цен
 for price_element in prices:
     pr = price_element.text
     pr_clear = int(pr.replace('руб.', '').replace(' ', ''))
     price.append(pr_clear)
 
+# Закрытие драйвера
 driver.quit()
 
+# Создание DataFrame и сохранение цен в CSV-файл
 df = pd.DataFrame(price, columns=['Price'])
 df.to_csv('divan_prices.csv', index=False)
 
+# Вычисление средней цены и вывод на экран
 average_price = round(df['Price'].mean(), 2)
 print(f'Средняя цена на диваны: {average_price}')
 
+# Построение гистограммы цен на диваны
 plt.hist(price, bins=10, color='skyblue', edgecolor='black')
 plt.xlabel('Цена')
 plt.ylabel('Количество')
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
+# Импортируем библиотеки
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+# Генерация случайных данных с нормальным распределением
 data = np.random.normal(0, 1, 1000)
 
+# Построение гистограммы нормального распределения
 plt.hist(data, bins=20)
-plt.xlabel("x ось")
-plt.ylabel("y ось")
-plt.title("Гистограмма нормального распределения")
-plt.show()
+plt.xlabel("x ось")  # Название оси x
+plt.ylabel("y ось")  # Название оси y
+plt.title("Гистограмма нормального распределения")  # Заголовок графика
+plt.show()  # Отображение графика
 
-
+# Генерация случайных данных для диаграммы рассеяния
 x = np.random.rand(20)
 y = np.random.rand(20)
 
+# Построение диаграммы рассеяния
 plt.scatter(x, y)
-plt.xlabel("ось Х")
-plt.ylabel("ось Y")
-plt.title("Диаграмма рассеяния")
-plt.show()
+plt.xlabel("ось Х")  # Название оси X
+plt.ylabel("ось Y")  # Название оси Y
+plt.title("Диаграмма рассеяния")  # Заголовок графика
+plt.show()  # Отображение графика
